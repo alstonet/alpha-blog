53da1902c967254bc11e57e99b427dea834f7383
Rails Udemy Course <railsudemycourse@gmail.com>
2017-04-27 11:49:06 +0800

    Add user listing

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,8 +1,9 @@
 module ApplicationHelper
-  def gravatar_for(user, options = { size:80})
+  def gravatar_for(user, options = { size:150 } )
     gravatar_id = Digest::MD5::hexdigest(user.email.downcase)
     size = options[:size]
+    #default_url = "http://4.bp.blogspot.com/-SRSVCXNxbAc/UrbxxXd06YI/AAAAAAAAFl4/332qncR9pD4/s1600/default-avatar.jpg"
     gravatar_url = "https://secure.gravatar.com/avatar/#{gravatar_id}?s=#{size}"
-    image_tag(gravatar_url, alt: user.username, class: "img-circle")
+    image_tag(gravatar_url, alt: "user.username", class: "img-circle")
   end
 end
